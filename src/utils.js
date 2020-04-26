84e88411b9f06e94aa6d124f26baa54448b66807
Alejandro F <progressandro@gmail.com>
2020-04-26 19:22:13 -0400

    Auth flow finished.

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const APP_SECRET = 'hackernews-project'
+
+function getUserId(context) {
+  const Authorization = context.request.get('Authorization')
+  console.log(Authorization)
+  if (Authorization) {
+    const token = Authorization.replace('Bearer ', '')
+    const { userId } = jwt.verify(token, APP_SECRET)
+    return userId
+  }
+}
+
+module.exports = {
+  APP_SECRET,
+  getUserId,
+}
